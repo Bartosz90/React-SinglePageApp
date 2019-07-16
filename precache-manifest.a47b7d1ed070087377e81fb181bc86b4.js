6b218456b8abfb53ba67ca0a719527b1f14393b1
Bartek Krupa <krupa1.bk@gmail.com>
2019-07-16 22:54:58 +0200

    Updates

---
+++ precache-manifest.a47b7d1ed070087377e81fb181bc86b4.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e2039a71bf4bb21193f8fe64a2f2617",
+    "url": "/React-SinglePageApp/index.html"
+  },
+  {
+    "revision": "c74b6018e772a80f8c66",
+    "url": "/React-SinglePageApp/static/css/main.51ff6142.chunk.css"
+  },
+  {
+    "revision": "cc32b219b5505b83edc2",
+    "url": "/React-SinglePageApp/static/js/2.fcf04b7a.chunk.js"
+  },
+  {
+    "revision": "c74b6018e772a80f8c66",
+    "url": "/React-SinglePageApp/static/js/main.b0c36c63.chunk.js"
+  },
+  {
+    "revision": "54bc63db0964aabd376b",
+    "url": "/React-SinglePageApp/static/js/runtime~main.eb3b9e31.js"
+  },
+  {
+    "revision": "3e48597dbbff54051fe65e2661e3c535",
+    "url": "/React-SinglePageApp/static/media/homeimg1.3e48597d.png"
+  },
+  {
+    "revision": "d94c7d2be795c5884fe861275fa83698",
+    "url": "/React-SinglePageApp/static/media/homeimg2.d94c7d2b.png"
+  },
+  {
+    "revision": "f4da93105b8ce2aef416596b38fbbea8",
+    "url": "/React-SinglePageApp/static/media/homeimg3.f4da9310.png"
+  },
+  {
+    "revision": "94be9f51133cb2bb3d277377057c9ba0",
+    "url": "/React-SinglePageApp/static/media/map.94be9f51.png"
+  },
+  {
+    "revision": "28831ad7ff9afbe734accbcd9a01ea0b",
+    "url": "/React-SinglePageApp/static/media/work1.28831ad7.png"
+  },
+  {
+    "revision": "ec92e46b9b9ff379dbe6e9e6a59224c1",
+    "url": "/React-SinglePageApp/static/media/work2.ec92e46b.png"
+  },
+  {
+    "revision": "aeb1995d2a36d5a91a601f1aac483fdf",
+    "url": "/React-SinglePageApp/static/media/work3.aeb1995d.png"
+  },
+  {
+    "revision": "cec216f72e78784fde1b850644fb0157",
+    "url": "/React-SinglePageApp/static/media/work4.cec216f7.png"
+  },
+  {
+    "revision": "f1197896b84317a6fc4970032dae9084",
+    "url": "/React-SinglePageApp/static/media/work5.f1197896.png"
+  }
+]);
